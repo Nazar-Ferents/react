3638a358d6cc3d1b49ee4d474942e9421ccb1d8d
Nazar-Ferents <nazarferents594@gmail.com>
2026-03-04 15:43:26 +0200

    lesson 1 children

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import MyComponent from "./MyComponent/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-
+      <MyComponent title="MyComponent" >Lorem ipsum dolor.</MyComponent>
     </>
   )
 }
---
+++ src/MyComponent/MyComponent.tsx
@@ -0,0 +1,14 @@
+
+import type {FC, ReactNode} from 'react';
+
+type MyComponentPropType = { title: string;
+                             children?: ReactNode;}
+
+const  MyComponent: FC<MyComponentPropType> = ({title,children})=> {
+
+    return <div>
+        <h2>{title}</h2>
+        <p>{children}</p>
+    </div>
+}
+export default MyComponent;
