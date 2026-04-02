35fc84b6703f64a82096dedb5f19ced196cc3b2a
Nazar-Ferents <nazarferents594@gmail.com>
2026-04-02 18:11:36 +0300

    lesson 5 useForm. Базова валідація

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -10,20 +10,47 @@ interface IFormProps {
 const FormComponent = () => {
 
 
-    const {handleSubmit, register} = useForm<IFormProps>()
+    const {
+        handleSubmit,
+        register,
+        formState:{errors,isValid}
+    } = useForm<IFormProps>({
+        mode:'all'
+    });
 
-
-console.log({...register('username')})
-    const customHandler = (formDataProps:IFormProps) =>{
-        console.log(formDataProps)
-    };
+        const customHandler = (formDataProps:IFormProps) => {
+        console.log(formDataProps);
+        }
     return (
         <div>
             <form onSubmit={handleSubmit(customHandler)}>
-                <input type="text" {...register('username')}/>
-                <input type="text" {...register('password')}/>
-                <input type="number" {...register('age')}/>
-                <button>send</button>
+                <label>
+                    <input type="text" {...register('username',{
+                        required:true,
+                        pattern:{
+                            value:/\w+/,
+                            message:'wrong name'
+                        },
+                        minLength:{
+                            value:1,
+                            message:'wrong name'
+                        }
+
+                    })}/>
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+                <input type="text" {...register('password',{
+                    required:true,
+                    minLength:{value:3, message:'wrong password'},
+                    maxLength:{value:10, message:'wrong password'}
+                })}/>
+                <input type="number" {...register('age',{
+                    required:true,
+                    valueAsNumber:true,
+                    min:{value:1,message:'age too small'},
+                    max:{value:108,message:'age too big'}
+                })}/>
+                <button disabled={!isValid}>send</button>
             </form>
         </div>
     );
