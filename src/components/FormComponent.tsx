1d269698e0514339ed50672925725bd2e3d0b048
Nazar-Ferents <nazarferents594@gmail.com>
2026-04-02 16:19:29 +0300

    lesson 5 Контрольовані компоненти та форми

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,10 +1,47 @@
+import {type FormEvent, useState} from "react";
+
+
+
+interface IFormProps {
+    username:string,
+    password:string
+}
 
 const FormComponent = () => {
+
+    const [formState,setFormState] = useState<IFormProps>({
+        username:'default username',
+        password:'1111'
+    })
+
+   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        let user = {
+            username:formState.username,
+            password:formState.password};
+        console.log(user);
+
+   }
+
+    // const handleUsernameChange= (e:FormEvent<HTMLInputElement>)=> {
+    //       const input = e.target as HTMLInputElement;
+    //       console.log(input.value)
+    //     setFormState({...formState, username:input.value})
+    // }
+
+    const handleInputChange= (e:FormEvent<HTMLInputElement>) => {
+
+        const input = e.target as HTMLInputElement;
+        console.log(input.name)
+        setFormState({...formState, [input.name]:input.value})
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" name={'username'}/>
-                <input type="text" name={'password'}/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
+                <input type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+                <button>send</button>
             </form>
         </div>
     );
