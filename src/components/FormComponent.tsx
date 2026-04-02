4ead60e155dc77268e9a79809ef833e4bdb8ac59
Nazar-Ferents <nazarferents594@gmail.com>
2026-04-02 15:14:09 +0300

    lesson 5 НЕ Контрольовані компоненти та форми

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,13 @@
+
+const FormComponent = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+            </form>
+        </div>
+    );
+};
+
+export default FormComponent;
