fd9a6179d99a7c751856dc669ce485e200fd8f77
Nazar-Ferents <nazarferents594@gmail.com>
2026-04-20 20:06:01 +0300

    lesson 8 Redux Toolkit

--- src/redux/slices/userSlice/userSlice.ts
+++ src/redux/slices/userSlice/userSlice.ts
@@ -1,11 +1,14 @@
-import {createAsyncThunk, createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, isFulfilled, type PayloadAction} from "@reduxjs/toolkit";
 import type {IUser} from "../../../modules/IUser.ts";
 
 type UsersSliceType = {
     users:IUser[];
+    user:IUser | null;
+    loadState: boolean;
 }
 
-const initialState: UsersSliceType = {users: []}
+const initialState: UsersSliceType = {users: [],
+user:null, loadState: false};
 
 const loadUsers = createAsyncThunk(
     'userSlice/loadUsers',
@@ -14,6 +17,7 @@ const loadUsers = createAsyncThunk(
      try {
          const users =   await fetch('https://jsonplaceholder.typicode.com/users')
              .then(res => res.json())
+         // thunkAPI.dispatch(userSliceActions.changeLoadState(true));
          return thunkAPI.fulfillWithValue(users);
      }
      catch (error) {
@@ -22,10 +26,33 @@ const loadUsers = createAsyncThunk(
      }
     })
 
+const loadUser = createAsyncThunk(
+    'userSlice/loadUser',
+    async (id:string,thunkAPI) => {
+
+        try {
+            const user =   await fetch('https://jsonplaceholder.typicode.com/users/'+ id)
+                .then(res => res.json())
+            // thunkAPI.dispatch(userSliceActions.changeLoadState(true));
+
+            return thunkAPI.fulfillWithValue(user);
+        }
+        catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue('some error')
+        }
+    })
+
  export const userSlice = createSlice ({
     name: "userSlice",
     initialState: initialState,
-    reducers: {},
+    reducers: {
+
+        changeLoadState:(state,action:PayloadAction<boolean>) => {
+
+            state.loadState = action.payload;
+        }
+    },
      extraReducers: (builder) => {
         builder.addCase(loadUsers.fulfilled,(state, action:PayloadAction<IUser[]>) => {
             state.users = action.payload;
@@ -34,9 +61,15 @@ const loadUsers = createAsyncThunk(
                 console.log(state)
                 console.log(action.payload)
             })
+            .addCase(loadUser.fulfilled,(state, action:PayloadAction<IUser>) => {
+                state.user = action.payload
+            })
+            .addMatcher(isFulfilled(loadUser,loadUsers),(state) => {
+                state.loadState = true
+            })
      }
 })
 
 export const userSliceActions = {
-    ...userSlice.actions, loadUsers
+    ...userSlice.actions, loadUsers, loadUser
 }
